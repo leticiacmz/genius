11a39aeb45f4a2b7bcd75a4ff014c99523c126b3
leticiacmz <leticmz@gmail.com>
2021-08-14 01:01:00 -0300

    fix: problem with size, colors, time

--- script.js
+++ script.js
@@ -12,23 +12,20 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
-const nivel = document.getElementById('nivel');
-const pontos = document.getElementById('pontos');
-
-//cria ordem aletoria de cores
+//cria ordem aleatória de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
     clickedOrder = [];
 
-    for(let i in order) {
+    for(let i in order){
         let elementColor = createColorElement(order[i]);
         lightColor(elementColor, Number(i) + 1);
     }
 }
 
-//acende a proxima cor
-let lightColor = (element, number) => {
+//Acende a próxima cor
+lightColor = (element, number) => {
     number = number * 500;
     setTimeout(() => {
         element.classList.add('selected');
@@ -38,21 +35,21 @@ let lightColor = (element, number) => {
     }, number);
 }
 
-//verifica se os botoes clicados são os mesmos mostrados anteriormente
+//Checa se os botões clicados são os mesmos que a ordem gerada no jogo
 let checkOrder = () => {
     for(let i in clickedOrder) {
-        if(clickedOrder[i] != order[i]) {
+        if(clickedOrder[i] != order[i]){
             gameOver();
             break;
         }
     }
-    if(clickedOrder.length == order.length) {
-        alert(`Pontuação: ${score}\nVocê acertou! Próximo nível!`);
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
         nextLevel();
     }
 }
 
-//funçãp verificar o click do usuario
+//Função para o clique do usuário
 let click = (color) => {
     clickedOrder[clickedOrder.length] = color;
     createColorElement(color).classList.add('selected');
@@ -60,14 +57,14 @@ let click = (color) => {
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
         checkOrder();
-    },250);
+    }, 250);
 }
 
-//função que retorna a cor
+//Função que retorna a cor
 let createColorElement = (color) => {
     if(color == 0) {
         return green;
-    } else if(color == 1) {
+    } else if (color == 1) {
         return red;
     } else if (color == 2) {
         return yellow;
@@ -76,35 +73,34 @@ let createColorElement = (color) => {
     }
 }
 
-//função para proximo nivel do jogo
+//Função para próximo nível do jogo
 let nextLevel = () => {
     score++;
     shuffleOrder();
 }
 
-//função fim do jogo
+//Função para game over
 let gameOver = () => {
-    alert(`Pontuação: ${score}!\nVocê perdeu tudo!!!!!\nClique em OK para iniciar um novo jogo`);
+    alert(`Pontuação: ${score}\nVocê perdeu tudo!!!\nClique em ok para começar novamente`);
     order = [];
     clickedOrder = [];
 
     playGame();
 }
 
-//funcao de inicio do jogo
+//Função de início do jogo
 let playGame = () => {
-    alert('vamo lá eim testa tua memoria ai');
+    alert('Bem-vindo ao Genius! Teste sua memória!')
     score = 0;
 
     nextLevel();
 }
 
-//eventos de clique para as cores
+//Eventos de clique para as cores
 green.onclick = () => click(0);
 red.onclick = () => click(1);
 yellow.onclick = () => click(2);
 blue.onclick = () => click(3);
 
-
-//inicio do jogo
+//Início do jogo
 playGame();
